fec89112ae32ae622853f8e02819488a15c02a12
BeskonchinRV <BeskonchinRV@411D-9>
2023-05-06 11:57:15 +0700

    Добавьте файлы проекта.

---
+++ Delegate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            Delegate delegates = new Delegate();
+            Console.WriteLine("Введите число с плавающей запятой");
+            delegates.Print(double.Parse(Console.ReadLine()));
+        }
+    }
+}
